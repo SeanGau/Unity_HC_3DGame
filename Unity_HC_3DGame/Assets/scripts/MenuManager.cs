e1e925df269dcc1ee91249d2d0b977886bb24616
SeanGau <rrtw0627@gmail.com>
2020-08-26 21:42:29 +0800

    finish ready scene

---
+++ Unity_HC_3DGame/Assets/scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Tooltip("載入畫面")]
+    public GameObject panel;
+    [Tooltip("載入進度")]
+    public Text textLoading;
+    [Tooltip("載入bar")]
+    public Image barLoading;
+    [Tooltip("提示文字")]
+    public Text textReady;
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading()
+    {
+        panel.SetActive(true);
+        AsyncOperation ao = SceneManager.LoadSceneAsync("遊戲場景");
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            textLoading.text = "載入進度：" + (ao.progress/0.9f * 100).ToString("F2") + "%";
+            barLoading.fillAmount = ao.progress / 0.9f;
+            yield return null;
+
+            if(ao.progress == 0.9f)
+            {
+                textReady.gameObject.SetActive(true);
+                if (Input.anyKeyDown)
+                    ao.allowSceneActivation = true;
+            }
+        }
+    }
+}
