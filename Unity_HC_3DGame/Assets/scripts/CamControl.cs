1c7bd6f1018f2034c8b8264ee76c9c689544f75d
SeanGau <rrtw0627@gmail.com>
2020-09-07 21:39:35 +0800

    change char & finish control

---
+++ Unity_HC_3DGame/Assets/scripts/CamControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+    public float turn = 50f;
+    public Vector2 limit = new Vector2(30, -20);
+
+    private Quaternion rot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Track()
+    {
+        Vector3 posA = transform.position;
+        Vector3 posB = target.position;
+
+        transform.position = Vector3.Lerp(posA, posB, Time.deltaTime * speed);
+
+        rot.x -= Input.GetAxis("Mouse Y") * turn * Time.deltaTime;
+        rot.y += Input.GetAxis("Mouse X") * turn * Time.deltaTime;
+
+        rot.x = Mathf.Clamp(rot.x, limit.y, limit.x);
+        transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
+    }
+    void LateUpdate()
+    {
+        Track();
+    }
+}
---
+++ Unity_HC_3DGame/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Tooltip("移動速度"), Range(0, 1000)]
+    public float speed = 5;
+    [Tooltip("旋轉速度"), Range(0, 1000)]
+    public float turn = 5;
+    [Tooltip("攻擊力"), Range(0, 1000)]
+    public float attack = 20;
+    [Tooltip("血量"), Range(0, 1000)]
+    public float hp = 250;
+    [Tooltip("魔力"), Range(0, 1000)]
+    public float mp = 50;
+
+    public float exp;
+    public int lv = 1;
+
+    private Animator anim;
+    private Rigidbody rig;
+    private Transform cam;
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+        rig = this.GetComponent<Rigidbody>();
+        cam = GameObject.Find("攝影機根物件").transform;
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 pos = cam.forward * v + cam.right * h;
+
+        transform.forward = cam.forward;
+        rig.MovePosition(transform.position + pos * speed * Time.deltaTime);
+        anim.SetFloat("move", Mathf.Sqrt(h * h + v * v));
+    }
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+}
